9148f624139fc9684c41687a54b7391e225b07cc
Vinicius-Bitencourt-Pereira <viniciusbitencourtpereira@gmail.com>
2022-05-15 22:07:21 -0300

    Pagination

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,7 +2,6 @@ import {ReactComponent as StarFull} from 'assets/img/star-full.svg'
 import {ReactComponent as StarHalf} from 'assets/img/star-half.svg'
 import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg'
 import './style.css';
-import { resolveTxt } from 'dns';
 
 type Props = {
     score: number;
